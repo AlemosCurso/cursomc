7c4cec6199e6130fca81263724ede6c6e52984ff
Alan Lemos <alanrj.lemos@gmail.com>
2019-03-20 11:25:00 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/alemos/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.alemos.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alemos.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
